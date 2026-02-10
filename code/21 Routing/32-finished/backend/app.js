27036d5bba8c95d1b1efab35e1a6752c1d9ded41
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-02-11 01:33:15 +0530

    code updated

--- code/21 Routing/32-finished/backend/app.js
+++ code/21 Routing/32-finished/backend/app.js
@@ -1,23 +1,21 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-
-const eventRoutes = require('./routes/events');
+import express, { json } from "express";
+import eventRoutes from "./routes/events";
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.use('/events', eventRoutes);
+app.use("/events", eventRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.status || 500;
-  const message = error.message || 'Something went wrong.';
+  const message = error.message || "Something went wrong.";
   res.status(status).json({ message: message });
 });
 
